a6f3d5388c6b48930ce4fb966a51311104cce6f5
jimmykagzz <jimmykagzz@gmail.com>
2022-07-04 10:10:01 +0000

    print letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
