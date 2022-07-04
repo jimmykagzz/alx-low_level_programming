6ed247411bd7270207a50ab56c4763e1312cf700
jimmykagzz <jimmykagzz@gmail.com>
2022-07-04 11:36:15 +0000

    combination of 2-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int numb1, numb2;
 
-	for (numb1 = '0'; numb1 <= '98'; numb1++)
+	for (numb1 = '0'; numb1 <= 98; numb1++)
 	{
-		for (numb2 = numb1 + 1; numb2 <= '99'; numb2++)
+		for (numb2 = numb1 + 1; numb2 <= 99; numb2++)
 		{
 			putchar((numb1 / 10) + '0');
 			putchar((numb1 % 10) + '0');
@@ -21,7 +21,7 @@ int main(void)
 			putchar((numb2 / 10) + '0');
 			putchar((numb2 % 10) + '0');
 
-		if (numb1 == '98' && numb2 == '99')
+		if (numb1 == 98 && numb2 == 99)
 		{
 			continue;
 		}
