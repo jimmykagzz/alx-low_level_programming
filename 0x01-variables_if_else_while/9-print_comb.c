fb76d1794d8c65bf617efdabb31d1f18b965897a
jimmykagzz <jimmykagzz@gmail.com>
2022-07-04 10:29:44 +0000

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = '0'; numb <= '9'; numb++)
+	{
+		putchar(numb);
+		if (numb != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
