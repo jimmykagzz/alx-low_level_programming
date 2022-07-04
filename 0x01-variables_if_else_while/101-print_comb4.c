0ca1add025584909a3eaca515565adfbd53c1a95
jimmykagzz <jimmykagzz@gmail.com>
2022-07-04 11:10:05 +0000

    combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+		{
+			for (digit3 = digit2+1; digit3 <='9; digit++)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				if (digit1 < '7' && digit2 < '8' && digit3 <= '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
