4a5ecec5a0a643c417b3677093f5d9a0dbdfb3b9
jimmykagzz <jimmykagzz@gmail.com>
2022-07-04 09:51:31 +0000

    print alphabet without e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
