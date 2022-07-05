06156a477b2bd172cd39f0908aff2d4294b987f6
jimmykagzz <jimmykagzz@gmail.com>
2022-07-05 14:48:04 +0000

    determine if the input is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - determines lowercase alphabet
+ *
+ * Return: 1 if lowercase & 0 for anything else
+ */
+int _islower(int c)
+{
+	int c;
+	
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+}
