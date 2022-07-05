899d3f4b4462a19822edebb1f7af9594cceb936e
jimmykagzz <jimmykagzz@gmail.com>
2022-07-05 14:17:34 +0000

    print lowercase alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int letter;
 	int numb;
 
-	for numb (numb = 0; numb < 10; numb++)
+	for (numb = 0; numb < 10; numb++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
