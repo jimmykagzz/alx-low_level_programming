2be95d9e789a69088ce737c189611f2daf28b7e2
jimmykagzz <jimmykagzz@gmail.com>
2022-07-05 13:47:26 +0000

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+
+	return (0);
+}
