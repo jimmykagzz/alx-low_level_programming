287feff2a9fc3352e13e765c5c623a3cad51bb15
jimmykagzz <jimmykagzz@gmail.com>
2022-07-05 13:37:35 +0000

    Adding header file and first program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char jina[8] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(jina[i]);
+	}
+	_putchar('\n');
+}
