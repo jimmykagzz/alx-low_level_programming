2e87bd805a1521d1e08a1665bd1071d3180cd753
jimmykagzz <jimmykagzz@gmail.com>
2022-07-05 18:56:47 +0000

    get sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - get sum
+ *
+ * @numb1: first number input
+ * @numb2: second number input
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int numb1, int numb2)
+{
+	return (numb1 + numb2);
+}
