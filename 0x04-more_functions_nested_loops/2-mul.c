db3b6c2b1f2436ed4dc1bad7a9c51344bf673c70
jimmykagzz <jimmykagzz@gmail.com>
2022-07-07 18:43:23 +0000

    determines product of 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: single character count
+ * @b: single character count
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = (a * b);
+	return (prod);
+}
+
