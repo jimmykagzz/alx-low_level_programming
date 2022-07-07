932ee4e8e3295a57d908d1694c0a0d85205f07ec
jimmykagzz <jimmykagzz@gmail.com>
2022-07-07 18:51:48 +0000

    prints 0-9wq

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from  0-9
+ *
+ *
+ * Return: ALways 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+}
+
