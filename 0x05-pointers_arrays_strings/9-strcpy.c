46dac45b6eae082459ca0fc0ed7adced53ac7781
jimmykagzz <jimmykagzz@gmail.com>
2022-07-13 01:40:17 +0000

    copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 /**
- * _strcpy - a function that copies the string pointed to by src,to the buffer pointed to by dest.
+ * _strcpy - a function that copies the string pointed to by src,
+ * to the buffer pointed to by dest.
  *
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
